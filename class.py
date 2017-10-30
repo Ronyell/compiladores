256d60daf8dc81772234a6fdee14833bf437f49a
Thiago Nogueira <thiagonf10@gmail.com>
2017-10-30 21:27:55 -0200

    Adding class.py

---
+++ class.py
@@ -0,0 +1,15 @@
+class Comment():
+    body = None
+
+
+class SubSection():
+    title = None
+    attribute = None
+    comment = []
+
+
+class Section():
+    title = None
+    subSection = []
+    comment = []
+    attribute = None
